65df1932b54669d55c4768ac8b883cd550638a47
unknown <vuples39@gmail.com>
2020-07-16 13:51:18 +0800

    add gray

--- test1.py
+++ test1.py
@@ -6,9 +6,13 @@ img=cv2.imread('Ch1.jpg')
 head=img[150:577,378:750]
 bb=img[100:527,100:472]
 dst=cv2.addWeighted(bb,0.7,head,0.3,0)
-#grayImg=cv2.imread('Ch1.jpg')
-print(img)
+img[100:527,100:472]=dst
 
-cv2.imshow('res',dst)
+cv2.imshow('res',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+''' gray 
+grayImg=cv2.imread('Ch1.jpg',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('gray',grayImg)
+'''
